478468f7ad2666fac6fe697a8a838ea1f8074086
huubve <huubve@yahoo.com>
2026-02-06 17:15:40 +0100

    array1.c is much, much better now ! 8-)

--- huub/show2/src/array1.c
+++ huub/show2/src/array1.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 /* the old way of arranging SPOT about the size of your array was with a define */
-#define MAX_CHILDREN 30
+#define MAX_X 13
 
 
 int main(int argc, char *argv[argc+1]) {
 	unsigned int	i;
-	int		child_ids[MAX_CHILDREN];
+	int		xs[MAX_X];
+	printf("line: %3d\n", __LINE__);
+	printf("------------------------------\n");
+	printf(" i  64*i*i*i>>i\n");
+	printf("------------------------------\n");
+	i = 0;
+	while ( i < MAX_X ) {
+		/* why? Dunno. */
+		/* the old way of arranging SPOT about the size of your array was with a define */
+		xs[i] = (64 * i * i * i ) >> i;
+		printf("%2d  %6d\n", i, xs[i]);
+		i++;
+	}
+	printf("------------------------------\n\n\n");
 
 	int		nrs[] = { 5, 7, 11, 13, 17, 21};
 	/* the way to get the length of an array at compiletime is
@@ -18,14 +31,6 @@ int main(int argc, char *argv[argc+1]) {
 	unsigned char	uc_squares[sizeof(nrs)/sizeof(nrs[0])];
 
 	int		n_nrs = sizeof(nrs)/sizeof(nrs[0]);
-
- 	/* in C, type char is nothing more and nothing less that an 8 bit int,
-	 * look further down somewhere to see how we print them
-	 */
-	char		name[] = { 67, 101, 103, 101, 107, 97 }; /* yes, small ints can be a char */
-	int		n_name = sizeof(name)/sizeof(name[0]);
-	char		ascii_digits[]= { 48, 49,'2', '3', 52, 53, 54, '7', '8', '9' }; /* yes, char and small ints are interchangable */
-	int		n_digits = sizeof(ascii_digits)/sizeof(ascii_digits[0]);
 
 
 	i = 0;
@@ -91,6 +96,10 @@ int main(int argc, char *argv[argc+1]) {
 
 
 
+ 	/* in C, type char is nothing more and nothing less that an 8 bit int */
+	char		name[] = { 67, 101, 103, 101, 107, 97 }; /* yes, small ints can be a char */
+	int		n_name = sizeof(name)/sizeof(name[0]);
+
 	printf("line: %3d\n", __LINE__);
 	printf("------------------------------\n");
 	printf("printing char with %%d versus %%c\n");
@@ -121,6 +130,9 @@ int main(int argc, char *argv[argc+1]) {
 	printf("------------------------------\n\n\n");
 
 
+ 	/* in C, type char is nothing more and nothing less that an 8 bit int */
+	char		ascii_digits[]= { 48, 49,'2', '3', 52, 53, 54, '7', '8', '9' }; /* yes, char and small ints are interchangable */
+	int		n_digits = sizeof(ascii_digits)/sizeof(ascii_digits[0]);
 	
 	printf("line: %3d\n", __LINE__);
 	printf("------------------------------\n");
@@ -196,16 +208,16 @@ int main(int argc, char *argv[argc+1]) {
 	printf("------------------------------\n");
 	long la = 13579113;
 	long lb = 310197;
-	printf("la=%10ld  lb=%10ld\n", la, lb);
-	printf("la ^= lb\n");
+	printf("a=%10ld  b=%10ld\n", la, lb);
+	printf("a = a^b\n");
 	la ^= lb;
-	printf("la=%10ld  lb=%10ld\n", la, lb);
-	printf("lb ^= la\n");
+	printf("a=%10ld  b=%10ld\n", la, lb);
+	printf("b = b^a\n");
 	lb ^= la;
-	printf("la=%10ld  lb=%10ld\n", la, lb);
-	printf("la ^= lb\n");
+	printf("a=%10ld  b=%10ld\n", la, lb);
+	printf("a = a^b\n");
 	la ^= lb;
-	printf("la=%10ld  lb=%10ld\n", la, lb);
+	printf("a=%10ld  b=%10ld\n", la, lb);
 	printf("------------------------------\n\n\n");
 	return 0;
 }
