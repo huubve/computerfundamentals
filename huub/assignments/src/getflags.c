4c473f0de1aa993ec031df7d85cdddfa32192e36
huubve <huubve@yahoo.com>
2026-03-17 22:21:59 +0100

    nextfewlinesofgetflags

--- huub/assignments/src/getflags.c
+++ huub/assignments/src/getflags.c
@@ -3,11 +3,7 @@
 
 extern int getoptions( int argc, char *argv[], char * optiondefinition);
 
-typedef enum {
-      LIST = 1, BOOL = 2, CHAR = 4, STRING = 8, I32 = 16, UI64 = 32, DOUBLE = 64
-} value_type_e;
-
-
+/* value_u holds a single flag argument value */
 typedef union {
 		bool	b;
 		char	c;
@@ -17,9 +13,11 @@ typedef union {
 		double	d;
 } value_u;
 
+/* flag_t describes a single flag
+ */
 typedef struct	flag_s {
 	char		flag;
-	bool		found;
+	bool		given;
 	char		vformat[32];
 	value_type_e	vtype;
 	value_u		value;
@@ -28,24 +26,36 @@ typedef struct	flag_s {
 	value_u *	values;
 } flag_t;
 
-#define MAXFLAGS 129
-static flag_t	flags[MAXFLAGS];
+/* bits of this enum tell which member of value_u is used for the flag's argument
+ * and whether the flag argument is a single value or a list */
+typedef enum {
+      LIST = 1, BOOL = 2, CHAR = 4, STRING = 8, I32 = 16, UI64 = 32, DOUBLE = 64
+} value_type_e;
+
+#define MAXFLAGS 128
+static flag_t *	flags[MAXFLAGS]; /* one for every ASCII(7 bits) character */
 static bool flags_initialised=false;
 
 int	initflags(char * definition) {
 	
+	if ( flags_initialised ) return 1;
+
 	for ( int i = 0; i < MAXFLAGS; i++ ) {
-		flags[i].flag = (char)0;
-		flags[i].given = false;
-		flags[i].vformat[0] = (char)0;
-		flags[i].vseparator = (char)0;
-		flags[i].vtype = 0;
-		flags[i].vcount = 0;
-		flags[i].values = (value_u *)0;
 	}
 
-	for (; *definition; definition++) {
-		flags[*definition] = true;
+	while (*definition) {
+		char flag = *definition;
+      		char *p = definition;
+      		if (! isalnum(flag) ) return 0;
+      		flags[flag] = (flag_t *)malloc(sizeof(flag_t));
+		flags[flag]->flag = (char)flag;
+		flags[flag]->given = false;
+		flags[flag]->vformat[0] = (char)0;
+		flags[flag]->vseparator = (char)0;
+		flags[flag]->vtype = 0;
+		flags[flag]->vcount = 0;
+		flags[flag]->values = (value_u *)0;
+      		definition++;
 	}
 
 }
